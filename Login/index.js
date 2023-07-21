9aac10e41f2c5d88f2b1db4d1ecea98856b325fb
LÊ VĂN MẠNH <levanmanh20012000@gmail.com>
2023-07-21 21:07:07 +0700

    buoi1

---
+++ Login/index.js
@@ -0,0 +1,15 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+import styles from './styleLogin.js'
+   const Login=()=>{
+  return (
+      <View style={styles.LoginStyle}>
+        <Text>index</Text>
+      </View>
+    )
+   }
+  
+   export default Login;
+  
+
+
---
+++ Login/styleLogin.js
@@ -0,0 +1,9 @@
+  import { Text, StyleSheet, View } from 'react-native'
+  import React, { Component } from 'react'
+  
+   const styles= StyleSheet.create({  
+        LoginStyle:{
+         flex:1,
+         backgroundColor:'red'})
+  
+   export default styles;
